33dffc41d5a485166710465ed2d782f97663f37e
tammarGrossman <tammar0583254978@gmail.com>
2022-10-27 18:51:13 +0300

    initial code+ welcome functions

---
+++ stage0/Program6185.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace stage0
+{
+   partial  class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome6185();
+            welcome7827();
+            Console.ReadKey();
+        }
+         static partial void welcome7827()
+        {
+            string name;
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        private static void welcome6185()
+        {
+            string name;
+            Console.Write("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
