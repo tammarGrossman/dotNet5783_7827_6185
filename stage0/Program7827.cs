24741efa16687f9f2bafc50593544da1ee8af5c8
100michal100 <michalsegal23@gmail.com>
2022-10-28 11:07:14 +0300

    i add a new class

---
+++ stage0/Program7827.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace stage0
+{
+    partial class Program
+    {
+        static partial void welcome7827()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
+
