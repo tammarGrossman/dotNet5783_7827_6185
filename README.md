acfc46ffcdb6595e49fc02df0b795179f2e65b3f
100michal100 <michalsegal23@gmail.com>
2022-10-28 12:13:29 +0300

    helloo world

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 hello my name is tammar
 # dotNet5783_7827_6185
+Hello World
--- stage0/Program7827.cs
+++ stage0/Program7827.cs
@@ -5,7 +5,7 @@ namespace stage0
 {
     partial class Program
     {
-        static partial void welcome7827()
+        static void welcome7827()
         {
             Console.WriteLine("I am also here!");
             Console.WriteLine("I am so happy for you!");
